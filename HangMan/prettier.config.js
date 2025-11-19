6e22b0014d1f2de7ced2c59ac9cde633aa7c5961
Iverson Jose <jiverson093@gmail.com>
2025-11-19 20:13:32 +0200

    Add files via upload

---
+++ HangMan/prettier.config.js
@@ -0,0 +1,19 @@
+export default {
+  //Use single quotes instead of double quotes
+  singleQuote: true,
+
+  //Always add semicolons at the end of statements
+  semi: true,
+
+  //Wrap lines after 100 characters
+  printWidth: 100,
+
+  //Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  trailingComma: 'es5',
+
+  //Keep arrow functions clean
+  arrowParens: 'avoid',
+
+  //Consistent spacing inside objects
+  bracketSpacing: true,
+};
